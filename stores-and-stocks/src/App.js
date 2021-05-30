3932c150f51fae2b408db95e570450337957965a
mondar88 <52878910+mondar88@users.noreply.github.com>
2021-05-30 11:54:41 +0530

    feature search results

--- stores-and-stocks/src/App.js
+++ stores-and-stocks/src/App.js
@@ -2,12 +2,10 @@ import './App.css';
 import {useState} from 'react';
 
 
-var message = " ";
-
 const inventory ={ flipflops:[
                               {Location: "Sainikpuri",   name: "Bata Shoes", available:true}, 
-                              {Location: "Secunderabad", name:"Happy Shoes", available:false}, 
-                              {Location: "Madhapur",     name: "Max Store", available:true}, 
+                              {Location: "Secunderabad", name:"Happy Shoes", available:true}, 
+                              {Location: "Madhapur",     name: "Max Store", available:false}, 
                             ],
                    laptops:[
                               {Location: "Secunderabad", name: "Satyam Electronics", available:false}, 
@@ -24,39 +22,59 @@ const inventory ={ flipflops:[
 
 
 function App() {
-  const [item, setItem] = useState("flipflops");
-
-  var options =[];
-
+  const [item, setItem] = useState("dummyItem");
+  const [loc, setLoc] = useState("dummyLoc");
+  const[message, setMessage] = useState("");
+  var match = false;
 
     function itemHandler(event){
       const newItem = event.target.value;
-      //console.log("input:> "+event.target.value)
       setItem(newItem);
-      //console.log("newItem< "+item);
-      message ="Hold on we are searching for the nearest store selling ";
-      
-      
+    }
+
+    function locHandler(event){
+      const newloc = event.target.value;
+      setLoc(newloc);
+      var newMessage = "searching for " + item;
+      setMessage(newMessage);
     }
 
     function storeSearch(event){
-      //console.log(item);
-      //console.log(event.target.value);
-     // const loc = event.target.value;
-      const goodsfun = Object.keys(inventory).map((goods)=>{
+      //const goodsfun = 
+      Object.keys(inventory).map((goods)=>{
         if(goods === item){
           console.log(inventory[goods]);
           //var options = goods;
-         inventory[item].map((store) => (
-            console.log(store.name)
-         ))
+         inventory[item].map((store) => {
+          var place = (store.Location);
+           if(place === loc){
+             if(store.available){
+              match = true;
+              var newMessage = "your nearest location for " + item + " in " + loc + " is " + store.name;
+              setMessage(newMessage);
+             }
+             else{
+              match = true;
+              newMessage = "currently out of stock ";
+              setMessage(newMessage);
+             }
+            
+           }
+
+           
+          
+           return(0);
+         })
           
         }
         return(0)
         
       });
           
-      
+      if(!match){
+        var newMessage = "Sorry! couldn't find anything ☹️"
+        setMessage(newMessage);
+      }
       
     }
 
@@ -64,8 +82,9 @@ function App() {
     <div className="App">
       <h1>Welcome to stock-and-stores</h1>
       <input onChange ={itemHandler} placeholder="item you are looking for"></input> 
-      <input onChange ={storeSearch} placeholder="your location"></input>
-      <h5> {message} </h5> {item}
+      <input onChange ={locHandler} placeholder="your location"></input>
+      <button type="submit" onClick={storeSearch}>Search</button>
+      <h5> {message} </h5> 
 
       
     </div>
